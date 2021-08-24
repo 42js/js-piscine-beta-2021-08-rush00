d1ffd16c4b6194bfa8cb6ac787cc3ff70e1e6e69
Yenowme <76904042+Yenowme@users.noreply.github.com>
2021-08-24 22:43:09 +0900

    feat: md editor by toastui

--- client/src/components/CreateArticlePage/CreateArticlePage.js
+++ client/src/components/CreateArticlePage/CreateArticlePage.js
@@ -1,7 +1,40 @@
-import React from "react";
+import React, { useState, useRef } from "react";
+import "@toast-ui/editor/dist/toastui-editor.css";
+//import "@toast-ui/editor/dist/toastui-viwer.css";
+import { Button } from "antd";
+import { Editor } from "@toast-ui/react-editor";
+import ReactMarkdown from "react-markdown";
 
 function CreateArticlePage() {
-    return <div></div>;
+    const editorRef = useRef();
+
+    const [content, setContent] = useState("");
+
+    const handleEditorBtn = (e) => {
+        const editorInstace = editorRef.current.getInstance();
+        const getContent_md = editorInstace.getMarkdown();
+        const getContent_html = editorInstace.getHTML();
+        setContent(getContent_md);
+        console.log(getContent_md);
+        console.log(getContent_html);
+        console.log(content);
+    };
+    return (
+        <div>
+            <Editor
+                initialValue="hello react editor world!"
+                previewStyle="vertical"
+                height="600px"
+                initialEditType="markdown"
+                useCommandShortcut={true}
+                ref={editorRef}
+            />
+            <Button type="primary" onClick={handleEditorBtn}>
+                저장하기
+            </Button>
+            <ReactMarkdown>{content}</ReactMarkdown>
+        </div>
+    );
 }
 
 export default CreateArticlePage;
